d6ac4de3b09684236edddb24aa91d687662015d2
yaowenqiang <yaowenqiang111@163.com>
2017-05-28 19:47:17 +0800

    crate list_books view and template

--- book/views.py
+++ book/views.py
@@ -1,7 +1,17 @@
+from .models import Book
 from django.shortcuts import render
 # Create your views here.
 
 def list_books(request):
+    '''
+    List books that had reviews
+    '''
+    books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+    # books = Book.objects.exclude(date_reviewed__isnull=True)
+
+    context = {
+        'books':books
+    }
     # return HttpResponse('We could put anything here.')
     # return HttpResponse(request.user.username)
-    return render(request, '../templates/list.html')
+    return render(request, '../templates/list.html', context)
