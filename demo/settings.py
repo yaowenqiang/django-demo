c0054e1c7dc828efd538d5622d69a7a026e16a3e
yaowenqiang <yaowenqiang111@163.com>
2017-05-29 02:16:19 +0800

    add base.html view and book-detail.html view

--- demo/settings.py
+++ demo/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'book/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
